5523c3602266cf062cd7dc14acb21be4e91332fa
steve vallois-davies <stevie.vallois.davies@gmail.com>
2026-03-27 17:19:57 +0000

    choer: missing import

--- backend/internal/http/server.go
+++ backend/internal/http/server.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/svdx9/conjugate-cc/backend/internal/api/v1"
 	"github.com/svdx9/conjugate-cc/backend/internal/status"
 )
